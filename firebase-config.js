7d18d2b72922ea9e8cdf7e4c6dcf8f43928a1e97
claireee1114 <yyryingru@gmail.com>
2026-05-24 08:49:11 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,15 @@ export const ADMIN_EMAIL = "yingru_94@hotmail.com";
 
 export const STORAGE_FOLDER = "footprint-images";
 
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDsBfPtha_0H5jZyugTY8vzyAfcr_VC9lU",
   authDomain: "xiaoyu-satellite.firebaseapp.com",
   projectId: "xiaoyu-satellite",
@@ -11,3 +19,7 @@ export const firebaseConfig = {
   appId: "1:751515569392:web:bca563247d8a91f26dc635",
   measurementId: "G-PWV7K0N71M"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
